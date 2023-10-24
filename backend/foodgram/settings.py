9dee8728313ff0c745855c65327aeeca4ec79a2b
Aleksandr Bynkov <bixber.commercial@yandex.ru>
2023-10-24 18:43:02 +0300

    Create all endpoint for the user

--- backend/foodgram/settings.py
+++ backend/foodgram/settings.py
@@ -29,7 +29,8 @@ INSTALLED_APPS = [
     'rest_framework.authtoken',
     'djoser',
     'users.apps.UsersConfig',
-    'recipes.apps.RecipesConfig'
+    'recipes.apps.RecipesConfig',
+    'api.apps.ApiConfig',
 ]
 
 MIDDLEWARE = [
